aca13c2ea1b41ebb7e698e0c80de6333d7d87cf0
Robbie Ribeiro <robbie@codestream.co.za>
2021-05-27 13:41:03 +0200

    New Introduction Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
